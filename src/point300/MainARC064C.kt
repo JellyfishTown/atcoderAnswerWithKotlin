cd2ec119c9e55aea14b16179204423827541a4dc
mizuki.uemura <mizuki.uemura@accenture.com>
2018-08-22 18:03:56 +0900

    update

---
+++ src/point300/MainARC064C.kt
@@ -0,0 +1,7 @@
+package point300
+
+fun main(args: Array<String>) {
+    val (n, x) = readLine()!!.split(' ').map(String::toInt)
+    val aList = readLine()!!.split(' ').map(String::toInt)
+
+}
---
+++ src/point300/MainARC066C.kt
@@ -0,0 +1,18 @@
+package point300
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.toInt()
+    val aList = readLine()!!.split(' ').map(String::toInt).sorted()
+    val mod = n % 2
+    for ((i, a) in aList.withIndex()) {
+        if ((a != i && (a - 1) != i) || a % 2 == mod) {
+            println(0)
+            return
+        }
+    }
+    var answer = 1L
+    for (i in 1..(n / 2)) {
+        answer = (answer * 2L) % 1000000007
+    }
+    println(answer)
+}

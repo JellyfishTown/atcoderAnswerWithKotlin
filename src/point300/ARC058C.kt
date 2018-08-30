6a1ac70ddd99fb8742becab60db388ac69adb1cc
mizuki.uemura <mizuki.uemura@accenture.com>
2018-08-31 08:21:36 +0900

    update

---
+++ src/point300/ARC058C.kt
@@ -0,0 +1,14 @@
+package point300
+
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.split(' ').map(String::toInt).first()
+    val dList = readLine()!!.split(' ').map { it.toCharArray().first() }
+    for (i in n until 100000) {
+        if(i.toString().toCharArray().all { dList.contains(it).not() }){
+            println(i)
+            break
+        }
+    }
+}
+

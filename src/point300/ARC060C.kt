1ef73d4f6a895513cccf826463db0473f292ad2f
mizuki.uemura <mizuki.uemura@accenture.com>
2018-08-31 10:29:54 +0900

    update

--- src/point300/ARC060C.kt
+++ src/point300/ARC060C.kt
@@ -1,32 +1,15 @@
 package point300
 
-import com.sun.xml.internal.ws.streaming.XMLStreamReaderUtil.close
-
-
-fun mainb(args: Array<String>) {
-    val (n, a) = readLine()!!.split(' ').map(String::toInt)
-    val xList = readLine()!!.split(' ').map(String::toInt)
-    val yList = xList.map { it - a }
-    val maxX = Math.max(xList.max()!!, a)
-    val dp = Array(n) { Array(2 * n * maxX!!) { 0 } }
-
-    for (j in 0 until n) {
-        for (t in 0 until 2 * n * maxX!!) {
-
-        }
-    }
-
-}
-
 fun main(args: Array<String>) {
     val (n, a) = readLine()!!.split(' ').map(String::toInt)
     val x = readLine()!!.split(' ').map(String::toInt)
-    val dp = Array(n + 1) { LongArray(x.max()!! * n + 1) { 0L } }
+    val max = x.max()!!
+    val dp = Array(n + 1) { LongArray(max * n + 1) { 0L } }
     dp[0][0] = 1
     for (i in 0 until n)
         for (j in i downTo 0)
-            for (k in 0..(x.max()!! * i)) {
-                println("i:$i,j:$j,k:$k,dp:${dp[j][k]}")
+            for (k in 0..(max * i)) {
+//                println("i:$i,j:$j,k:$k,dp:${dp[j][k]}")
                 dp[j + 1][k + x[i]] += dp[j][k]
             }
 
--- src/point300/ARC061C.kt
+++ src/point300/ARC061C.kt
@@ -2,10 +2,24 @@ package point300
 
 fun main(args: Array<String>) {
     val str = readLine()!!
-    var result: Long = 0
+    var result = 0L
+    val length = str.length
+
+    Integer.toBinaryString(0)
+
+    for(i in 0 until (Math.pow(2.0,length.toDouble() - 1.0).toInt())){
+        println(Integer.toBinaryString(i))
+    }
+
+
+}
+
+fun mainb(args: Array<String>) {
+    val str = readLine()!!
+    var result = 0L
 
     for (i in 0 until str.length) {
-        val no = Integer.parseInt(str[i].toString())
+        val no = str[i].toInt()
         for (j in 0 until str.length - i) {
             val temp = (no.toDouble()
                     * Math.pow(10.0, j.toDouble())
@@ -13,7 +27,7 @@ fun main(args: Array<String>) {
                     * Math.pow(2.0, Math.max(str.length - 2 - j - i, 0).toDouble())
                     ).toLong()
             result += temp
-            //println("i:$i,j:$j,no:$no,temp:$temp")
+            println("i:$i,j:$j,no:$no,temp:$temp")
         }
     }
     println(result)

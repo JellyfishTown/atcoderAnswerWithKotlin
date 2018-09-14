7e337fe20dfc154c5a1300f6cd5364a8fb94f4d7
mizuki.uemura <mizuki.uemura@accenture.com>
2018-09-14 18:18:07 +0900

    update

--- src/past/point300/AGC016A.kt
+++ src/past/point300/AGC016A.kt
@@ -1,16 +1,26 @@
 fun main(args: Array<String>) {
     val s = readLine()!!
-    val typeNum = s.toCharArray().toSet().size
-    var length = s.length
-    var maxNum = s.toCharArray().groupBy { it }.maxBy { it.value.size }!!.value.size
-    var i = 0
-    while (true) {
-        if (length <= maxNum) {
-            println(i)
-            return
+    var minNum = Int.MAX_VALUE
+    val sDis = s.toCharArray().distinct()
+    for (c in sDis) {
+        var temp = s.toMutableList()
+        var count = 0
+        while (true) {
+            val newTemp = mutableListOf<Char>()
+            if (temp.distinct().size == 1) {
+                break
+            }
+            for (i in 0 until temp.size - 1) {
+                if (temp[i] == c) {
+                    newTemp.add(temp[i])
+                } else {
+                    newTemp.add(temp[i + 1])
+                }
+            }
+            count++
+            temp = newTemp
         }
-        i++
-        length--
-        maxNum *= 2
+        minNum = Math.min(minNum, count)
     }
+    println(minNum)
 }

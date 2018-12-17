d993c367a339136723d6b667a0239a4be1780a18
mizuki.uemura <mizuki.uemura@accenture.com>
2018-12-17 13:09:57 +0900

    update

--- src/past/point300/ARC060C.kt
+++ src/past/point300/ARC060C.kt
@@ -6,9 +6,9 @@ fun main(args: Array<String>) {
     val max = x.max()!!
     val dp = Array(n + 1) { LongArray(max * n + 1) { 0L } }
     dp[0][0] = 1
-    for (i in 0 until n)
-        for (j in i downTo 0)
-            for (k in 0..(max * i)) {
+    for (i in 0 until n)//カードを1枚ずつループ
+        for (j in i downTo 0)//そのカードを1枚目に選ぶとき、2枚目に選ぶとき、3枚目に選ぶときをループ
+            for (k in 0..(max * i)) {//パターンを配るループ
 //                println("i:$i,j:$j,k:$k,dp:${dp[j][k]}")
                 dp[j + 1][k + x[i]] += dp[j][k]
             }

9ded2febf964ecefbc73534c5f95635f968a7a0c
mizuki.uemura <mizuki.uemura@accenture.com>
2019-05-09 11:19:22 +0900

    update

---
+++ src/past/point400/ABC124D.kt
@@ -0,0 +1,8 @@
+package past.point400
+
+
+fun main(args: Array<String>) {
+    val (n, k) = readLine()!!.split(" ").map(String::toLong)
+    val a = readLine()!!.split(" ").map(String::toLong)
+
+}

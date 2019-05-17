1b4186350c762b1172ab2e53ef6f23cf0486a895
mizuki.uemura <mizuki.uemura@accenture.com>
2019-05-17 18:24:32 +0900

    update

--- src/past/other/educationaldp/C.kt
+++ src/past/other/educationaldp/C.kt
@@ -2,17 +2,28 @@ package past.other.educationaldp
 
 fun main(args: Array<String>) {
     val n = readLine()!!.toInt()
-    val input = mutableMapOf<Int, List<Long>>()
-    (1..n).forEachIndexed { index, i -> input[index] =  readLine()!!.split(' ').map(String::toLong)}
+    val input = Array(n) { readLine()!!.split(' ').map(String::toLong).toLongArray() }
     val dp = Array(n) { LongArray(3) { 0L } }
-    dp[0][0] = input[0]!![0]
-    dp[0][1] = input[0]!![1]
-    dp[0][2] = input[0]!![2]
-
+    //(0,1,2)=(a,b,c)
+    (0..2).forEach { dp[0][it] = input[0][it] }
     for (i in 1 until n) {
-        dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][2]) + input[i]!![0]
-        dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][2]) + input[i]!![1]
-        dp[i][2] = Math.max(dp[i - 1][0], dp[i - 1][1]) + input[i]!![2]
+        (0..2).forEach { dp[i][it] = Math.max(dp[i - 1][(it + 1) % 3], dp[i - 1][(it + 2) % 3]) + input[i][it] }
     }
-    println(dp[n-1].max())
+    println(dp[n - 1].max()!!)
 }
+//fun main(args: Array<String>) {
+//    val n = readLine()!!.toInt()
+//    val input = mutableMapOf<Int, List<Long>>()
+//    (1..n).forEachIndexed { index, i -> input[index] =  readLine()!!.split(' ').map(String::toLong)}
+//    val dp = Array(n) { LongArray(3) { 0L } }
+//    dp[0][0] = input[0]!![0]
+//    dp[0][1] = input[0]!![1]
+//    dp[0][2] = input[0]!![2]
+//
+//    for (i in 1 until n) {
+//        dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][2]) + input[i]!![0]
+//        dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][2]) + input[i]!![1]
+//        dp[i][2] = Math.max(dp[i - 1][0], dp[i - 1][1]) + input[i]!![2]
+//    }
+//    println(dp[n-1].max())
+//}
--- src/past/other/educationaldp/D.kt
+++ src/past/other/educationaldp/D.kt
@@ -2,6 +2,27 @@ package past.other.educationaldp
 
 fun main(args: Array<String>) {
     val (n, w) = readLine()!!.split(' ').map(String::toInt)
+    val dp = Array(n + 1) { Array(w + 1) { 0L } }//dp[i][wi] i個目までの商品を選べる状況において総重量がwi以下の時の価値の最大値
+    (0 until n).forEach {
+        val (tw, v) = readLine()!!.split(' ').map(String::toLong)//tw:temporal weight
+        for (sw in 0..w) {//sw:sum weight
+            if (sw - tw >= 0) {//現在の品物を採択する場合
+                dp[it + 1][sw] = Math.max(
+                        dp[it + 1][sw]
+                        , dp[it][sw - tw.toInt()] + v)
+            } else {
+                dp[it + 1][sw] = Math.max(
+                        dp[it + 1][sw]
+                        , dp[it][sw])
+            }
+        }
+    }
+    println(dp[n][w])
+}
+
+
+fun main2(args: Array<String>) {
+    val (n, w) = readLine()!!.split(' ').map(String::toInt)
     val dp = Array(w + 1) { 0L }//dp[i] 総重量がi以下の時の価値の最大値
     (1..n).forEach {
         val (tw, v) = readLine()!!.split(' ').map(String::toLong)//tw:temporal weight
@@ -15,4 +36,3 @@ fun main(args: Array<String>) {
     }
     println(dp[w])
 }
-

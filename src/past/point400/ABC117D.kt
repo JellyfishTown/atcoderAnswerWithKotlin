8606be1d735bcb27cd420bf09d835dabbebed6c6
jellyfishTown <m-u816@live.jp>
2019-06-14 17:30:15 +0900

    update

--- src/past/point400/ABC117D.kt
+++ src/past/point400/ABC117D.kt
@@ -2,29 +2,35 @@ package past.point400
 
 fun main(args: Array<String>) {
     fun toBinaryNumberCharArray(num: Long, keta: Int): CharArray {
-        return String.format("%0${keta}d", java.lang.Long.toBinaryString(num).toLong()).toCharArray().reversedArray()
+        return String.format("%${keta}s", java.lang.Long.toBinaryString(num)).replace(" ", "0").toCharArray()
+                .reversedArray()
     }
 
-    val keta = 40
+    val keta = 41
+    val twoPow = (0..keta).map { it to Math.pow(2.0, it.toDouble()).toLong() }.toMap()
+
     val (n, k) = readLine()!!.split(' ').map(String::toLong)
     val aList = readLine()!!.split(' ').map { toBinaryNumberCharArray(it.toLong(), keta) }
     val standCount = IntArray(keta) { 0 }
     aList.forEach { a -> (0 until keta).forEach { if (a[it] == '1') standCount[it]++ } }
-    val k2 = toBinaryNumberCharArray(k, keta)
+    val kBin = toBinaryNumberCharArray(k, keta)
 
-    val dp = Array(keta) { Array(2) { "" } }//dp[keta][上位桁が全部一致フラグ]
-    var hasValue = false
+    val dp = Array(keta + 1) { Array(2) { 0L } }//dp[keta][上位桁が全部一致フラグ]
     for (i in (keta - 1) downTo 0) {
-        if (k2[keta] == '1') hasValue = true
-        if (!hasValue) continue
-
-
+        val stand = standCount[i]
+        val notStand = n.toInt() - stand
+        if (kBin[i] == '1') {
+            dp[i][0] = Math.max(
+                    dp[i + 1][1] + notStand * twoPow[i]!!, Math.max(
+                    dp[i + 1][0] + notStand * twoPow[i]!!,
+                    dp[i + 1][0] + stand * twoPow[i]!!)
+            )
+            dp[i][1] = dp[i + 1][1] + notStand * twoPow[i]!!
+        } else {
+            dp[i][0] = Math.max(dp[i + 1][0] + notStand * twoPow[i]!!,
+                    dp[i + 1][0] + stand * twoPow[i]!!)
+            dp[i][1] = dp[i + 1][1] + stand * twoPow[i]!!
+        }
     }
-    val a = 1L
+    println(Math.max(dp[0][0], dp[0][1]))
 }
-//            hasValue = true
-//            if(n.toDouble() / 2.0 >= standCount[keta].toDouble()){
-//                dp[keta][1]= "1"
-//            }else{
-//
-//            }

0a2342c4a248c9c6870f697a1ff8be66bb282be8
JellyfishTown <m-u816@live.jp>
2019-10-15 20:08:10 +0900

    update

--- src/past/other/internal/m6/1.kt
+++ src/past/other/internal/m6/1.kt
@@ -2,12 +2,23 @@ package past.other.internal.m6
 
 fun main(args: Array<String>) {
     val n = readLine()!!.toInt()
-    val map = Array(1001) { mutableSetOf<Int>() }
+
+    //添え字の頂点から延びる辺のSetを保持する配列
+    val branches = Array(1001) { mutableSetOf<Int>() }
+
+    //点a⇒b、b→aをそれぞれ保持
     (1 until n).forEach {
         val (a, b) = readLine()!!.split(' ').map(String::toInt)
-        map[a].add(b)
-        map[b].add(a)
+        branches[a].add(b)
+        branches[b].add(a)
     }
-    val max = map.map { it.size }.max()!!
-    println(max)
+
+    //1頂点から出る辺本数の最大値を取得する。
+    val maxBranchNum = branches.map { it.size }.max()!!
+
+    //n頂点、n-1本の辺で連結グラフの場合、ループ構造は確実に発生しない
+    //そのため点を削除するとそれに紐づく各頂点はそれぞれ異なる連結成分となる
+    println(maxBranchNum)
 }
+
+
--- src/past/other/internal/m6/2.kt
+++ src/past/other/internal/m6/2.kt
@@ -1,17 +1,22 @@
 package past.other.internal.m6
 
+//ほぼ愚直に数え上げるだけでOKだが、ループを2重にするとTLEするので多少改良する。
 fun main(args: Array<String>) {
     val n = readLine()!!.toInt()
-    val pList = (1..n).map { readLine()!!.toLong() }
-    val used = Array(n + 1) { false }
-    var ans = 0L
+    val pList = (1..n).map { readLine()!!.toInt() }
+
+    val usedNum = Array(n + 1) { false }//添字の数値が既に登場済かを保持する配列
+    var totalCount = 0 //組合せの総数をカウントする変数
+
     for (p in pList) {
-        var temp = 2 * p
-        used[p.toInt()] = true
-        while (temp <= n) {
-            if (used[temp.toInt()].not()) ans++
-            temp *= 2
+        usedNum[p] = true//p
+        var pCount = 0 //pより後ろ側に存在するp×(2^i)の数値の数をカウントする
+        var p2i = 2 * p //p×(2^i)
+        while (p2i <= n) {
+            if (usedNum[p2i].not()) pCount++//p×(2^i)が未使用であれば(=pより下側に存在する場合であれば)カウントアップ
+            p2i *= 2
         }
+        totalCount += pCount
     }
-    println(ans)
+    println(totalCount)
 }
